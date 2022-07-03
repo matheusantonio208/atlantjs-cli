c3a368182c128b37f3ac7a2a869ab1ce34ba4537
Matheus Antonio <matheus.silva-loldesign@fretebras.com.br>
2022-07-03 12:17:42 -0300

    add create command

---
+++ atlantjs/src/commands/create.ts
@@ -0,0 +1,82 @@
+module.exports = {
+  name: 'create',
+  description: 'Create the initial files of the application',
+  run: async (toolbox) => {
+    const {
+      parameters,
+      template,
+      print: { success, error },
+    } = toolbox
+
+    const name = parameters.first
+
+    if (!name) {
+      error('App name must be specified')
+      return
+    }
+
+    await template.generate({
+      template: 'core/.husky/pre-commit',
+      target: `${name}/.husky/pre-commit`,
+    })
+
+    await template.generate({
+      template: 'core/.vscode/launch.json',
+      target: `${name}/.vscode/launch.json`,
+    })
+
+    await template.generate({
+      template: 'core/.vscode/settings.json',
+      target: `${name}/.vscode/settings.json`,
+    })
+
+    await template.generate({
+      template: 'core/.editorconfig',
+      target: `${name}/.editorconfig`,
+    })
+
+    await template.generate({
+      template: 'core/.env.example.ejs',
+      target: `${name}/.env.example`,
+      props: { name },
+    })
+
+    await template.generate({
+      template: 'core/.eslintignore',
+      target: `${name}/.eslintignore`,
+    })
+
+    await template.generate({
+      template: 'core/.eslintrc.json',
+      target: `${name}/.eslintrc.json`,
+    })
+
+    await template.generate({
+      template: 'core/.gitignore',
+      target: `${name}/.gitignore`,
+    })
+
+    await template.generate({
+      template: 'core/.prettierrc',
+      target: `${name}/.prettierrc`,
+    })
+
+    await template.generate({
+      template: 'core/babel.config.js',
+      target: `${name}/babel.config.js`,
+    })
+
+    await template.generate({
+      template: 'core/README.md.ejs',
+      target: `${name}/README.md`,
+      props: { name },
+    })
+
+    await template.generate({
+      template: 'core/tsconfig.json',
+      target: `${name}/tsconfig.json`,
+    })
+
+    success(`Generated ${name} app.`)
+  },
+}
