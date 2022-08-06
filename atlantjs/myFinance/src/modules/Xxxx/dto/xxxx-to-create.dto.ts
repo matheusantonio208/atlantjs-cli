4ca32aa178b71b8f1e628955101cd4fb5e3a52b7
Matheus Antonio <matheus.silva-loldesign@fretebras.com.br>
2022-08-06 02:10:40 -0300

    asd

--- atlantjs/myFinance/src/modules/Xxxx/dto/xxxx-to-create.dto.ts
+++
@@ -1,7 +0,0 @@
-export class ToCreateDto {
-  property: string;
-
-  constructor(body: ToCreateDto) {
-    this.property = body?.property;
-  }
-}
--- atlantjs/src/commands/create:backend.ts
+++ atlantjs/src/commands/create:backend.ts
@@ -1,17 +1,20 @@
 import {
+  createFiles,
   createTempFiles,
   fileExists,
   parseJson,
   parseString,
-  createFiles,
-  save,
   removeTempFiles,
+  save,
 } from '../extends/file-manager'
+import { info } from 'console'
 import { resolve } from 'path'
+import { system } from 'gluegun'
 
 module.exports = {
   name: 'create:backend',
   description: 'Create the initial files of the application',
+
   run: async (toolbox) => {
     const {
       parameters,
@@ -19,8 +22,13 @@ module.exports = {
       print: { success, error },
     } = toolbox
 
-    const name: string = parameters.first ?? error('App name must be specified')
-    const backendFiles = createFiles('back-end/api', name)
+    const FOLDER_NAME_TEMPLATE = 'back-end/api'
+
+    const name: string = parameters.first || '.'
+
+    const { urlRepo } = toolbox.parameters.options
+
+    const backendFiles = createFiles(FOLDER_NAME_TEMPLATE, name)
 
     backendFiles.map(async (file) => {
       await createTempFiles(template, file)
@@ -29,13 +37,39 @@ module.exports = {
       const isFileUserExists = await fileExists(file)
 
       if (isFileUserExists) {
-        console.log('==MERGE FILES==')
+        console.log('==MERGE FILES (packages.json)==')
       }
 
       const fileTempString = await parseString(fileTempJson)
 
       save(resolve(file.target), fileTempString)
       await removeTempFiles()
+
+      info(`Run yarn install`)
+      await system.run(`cd ${name} && yarn`)
+
+      info(`Start git`)
+      await system.run(`
+      cd ${name} &&
+      git init &&
+      git add . &&
+      git commit -m "feat: add back-end layer layer" &&
+      git branch -M main
+    `)
+
+      if (urlRepo) {
+        await system.run(`
+        cd ${name} &&
+        git remote add origin ${urlRepo} &&
+        git push -u origin main
+      `)
+      }
+
+      await system.run(`
+      cd ${name} &&
+      code .
+    `)
+      success(`Generated ${name} app.`)
 
       //corrigir caminhos
       //deixar cli boita
